e667b9d354c9231a9daebbfb6f9ae058c6a8c6a8
Giga <gigakatamadze0@gmail.com>
2024-07-10 23:55:34 +0400

    Worked on arrays and slices through testing

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,15 +1,22 @@
 package main
 
-import "testing"
-
-func TestSum(t *testing.T) {
-
-	numbers := [5]int{1, 2, 3, 4, 5}
-
-	got := Sum(numbers)
-	want := 15
-
-	if got != want {
-		t.Errorf("got %d want %d given, %v", got, want, numbers)
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
 	}
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
 }
